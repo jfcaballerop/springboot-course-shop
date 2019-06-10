31f861e31b33d52f10182aaa83317b0d1ca7c364
Jose Caballero <josefernando.caballero@logesta.com>
2019-06-10 17:28:10 +0200

    add user class

---
+++ src/main/java/com/logesta/store/example/storeapplication/model/usuario.java
@@ -0,0 +1,15 @@
+package com.logesta.store.example.storeapplication.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * usuario
+ */
+@Entity
+@Table(name = "users")
+public class usuario {
+
+    @Id @Generated
+    
+}
